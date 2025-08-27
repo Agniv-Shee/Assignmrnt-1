d366cabb372afa21a833d21f6ad9bf6e51618435
Agniv-Shee <agnivshee48@gmail.com>
2025-08-27 08:43:11 +0530

    Add files via upload

---
+++ calculation of two no.py
@@ -0,0 +1,8 @@
+x=float(input("Enter the first no you want: "))
+y=float(input("enter the second no you want: "))
+print("The addition of two no is ",x+y)
+print("the substraction of two no is ",x-y)
+print("the multiplication of two no is ",x*y)
+print("the division of two no is ",x/y)
+
+
---
+++ name.py
@@ -0,0 +1,3 @@
+name1=input("enter the first name : ")
+name2=input("enter the last namee : ")
+print("Hello", name1+" "+name2,",welcome to the python programme")
